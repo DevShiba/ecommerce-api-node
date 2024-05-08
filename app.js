c2ce5c2d9b2d646cddd513bcdf33decec40a3f8f
DevShiba <migerushiba@gmail.com>
2024-05-07 23:57:44 -0300

    Auth Route

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const morgan = require('morgan')
 // database
 const connectBD = require("./db/connect");
 
+// routers
+const authRouter = require('./routes/authRoutes')
+
 // middleware
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -19,6 +22,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("e-commerce-api");
 });
+
+app.use('/api/v1/auth', authRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
