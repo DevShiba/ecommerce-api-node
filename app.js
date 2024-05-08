78573435e94154d637f60e103a18d29bc41ff58e
DevShiba <migerushiba@gmail.com>
2024-05-07 23:07:47 -0300

    AppJS

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('E-Commerce API');
+require("express-async-errors");
+
+// express
+const express = require("express");
+const app = express();
+
+// database
+const connectBD = require("./db/connect");
+
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("e-commerce-api");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectBD(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
