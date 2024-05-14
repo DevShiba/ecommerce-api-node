4a2c1992a85ef4afed8044b8037f529c50e7ad84
DevShiba <migerushiba@gmail.com>
2024-05-13 23:39:42 -0300

    Cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,9 +14,17 @@ const register = async (req, res) => {
 
   const isFirstAccount = (await User.countDocuments({})) === 0;
   const role = isFirstAccount ? "admin" : "user";
+
   const user = await User.create({ name, email, password, role });
   const tokenUser = {name: user.name, userId: user._id, role: user.role}
   const token = createJWT({payload: tokenUser})
+
+  const oneDay = 1000 * 60 * 60 * 24	
+
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay)
+  })
 
 
   res.status(StatusCodes.CREATED).json({ user });
