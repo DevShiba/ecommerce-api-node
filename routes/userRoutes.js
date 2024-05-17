0cf01abca12abdf43069e4634f8ea4716fffe58f
DevShiba <migerushiba@gmail.com>
2024-05-16 23:14:53 -0300

    Authentication Middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const { authenticateUser } = require("../middleware/authentication");
 
-const  {
+const {
   getAllUsers,
   getSingleUser,
   showCurrentUser,
   updateUser,
   updateUserPassword,
-} = require('../controllers/userController')
+} = require("../controllers/userController");
 
+router.route("/").get(getAllUsers);
 
-router.route('/').get(getAllUsers)
+router.route("/showMe").get(authenticateUser, showCurrentUser);
+router.route("/updateUser").patch(updateUser);
+router.route("/updateUserPassword").patch(updateUserPassword);
 
-router.route('/showMe').get(showCurrentUser)
-router.route('/updateUser').patch(updateUser)
-router.route('/updateUserPassword').patch(updateUserPassword)
+router.route("/:id").get(authenticateUser, getSingleUser);
 
-router.route('/:id').get(getSingleUser)
-
-module.exports = router
+module.exports = router;
