7cf8164dfa0631357708e881f37c1b11fe29bd2e
DevShiba <migerushiba@gmail.com>
2024-05-18 23:25:07 -0300

    Show Current User

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { authenticateUser } = require("../middleware/authentication");
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require("../middleware/authentication");
 
 const {
   getAllUsers,
@@ -10,7 +13,9 @@ const {
   updateUserPassword,
 } = require("../controllers/userController");
 
-router.route("/").get(getAllUsers);
+router
+  .route("/")
+  .get(authenticateUser, authorizePermissions("admin"), getAllUsers);
 
 router.route("/showMe").get(authenticateUser, showCurrentUser);
 router.route("/updateUser").patch(updateUser);
