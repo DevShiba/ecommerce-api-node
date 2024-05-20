f0b67646ebac1e5335dfe685d82d16cb9343adac
DevShiba <migerushiba@gmail.com>
2024-05-20 20:55:28 -0300

    UnauthorizedError

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -3,7 +3,7 @@ const CustomError = require("../errors");
 const checkPermissions = (requestUser, resourceUserId) => {
   if (requestUser.role === "admin") return;
   if (requestUser.userId === resourceUserId.toString()) return;
-  throw new CustomError.UnauthenticatedError("Not allowed");
+  throw new CustomError.UnauthorizedError("Not allowed");
 };
 
 module.exports = checkPermissions;
