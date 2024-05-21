45537c6c6c1dbcae4a522ea4d4041c29eb2df8fb
DevShiba <migerushiba@gmail.com>
2024-05-20 22:42:03 -0300

    Get All Products and Single Product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,25 +1,38 @@
+const Product = require('../models/product');
+const {StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors')
+
 const createProduct = async(req,res)=>{
-    res.send('create product')   
+    req.body.user = req.user.userId;
+    const product = await Product.create(req.body)
+    res.status(StatusCodes.CREATED).json({product})  
 }
 
 const getAllProducts = async (req, res) => {
-  res.send("get all product");
+    const products = await Product.find({})
+  res.status(StatusCodes.OK).json({products, count: products.length})
 };
 
 const getSingleProduct = async (req, res) => {
+    const {id: productId} = req.params;
+    const product = await Product.findOne({_id: productId});
+
+    if(!product){
+        throw new CustomError.NotFoundError(`No Product With Id: ${productId}`)
+    }
   res.send("get single product");
 };
 
 const updateProduct = async (req, res) => {
-  res.send("create product");
+  res.send("update product");
 };
 
 const deleteProduct = async (req, res) => {
-  res.send("create product");
+  res.send("delete product");
 };
 
 const uploadImage = async (req, res) => {
-  res.send("create product");
+  res.send("upload product");
 };
 
 module.exports ={
